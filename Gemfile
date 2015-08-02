7b2e6a524b27b0cb41c29b979a328c2a6c7e9897
kokosko <kostya.kokosko@gmail.com>
2015-08-02 10:08:00 +0300

    Users done

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'carrierwave'
 gem 'slim-rails'
 gem 'lazybox'
 gem 'bootstrap-sass'
+gem 'autoprefixer-rails'
 gem 'rails-i18n'
 gem 'simple_form'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :users
+    resources :users, :colors, :materials, :sizes, :products
   end
 end
