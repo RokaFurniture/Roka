e4649aa352f8cfe0c2423ca17a77e290a84ee359
kokosko <kostya.kokosko@gmail.com>
2015-08-24 12:10:26 +0300

    turn on Turbolinks, 'edit product' page

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,10 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
-  # Choose what kind of storage to use for this uploader:
-  # storage :file
-  # storage :fog
-
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
