bc11d8bcd359c2e43941f7e61f86307f688d28d8
kokosko <kostya.kokosko@gmail.com>
2015-10-20 11:33:08 +0300

    change input, output view, fix provider address column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151018140714) do
+ActiveRecord::Schema.define(version: 20151019175628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,7 +120,7 @@ ActiveRecord::Schema.define(version: 20151018140714) do
   create_table "providers", force: :cascade do |t|
     t.string "name"
     t.string "phone"
-    t.string "adress"
+    t.string "address"
   end
 
   create_table "sizes", force: :cascade do |t|
