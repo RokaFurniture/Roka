6c464f4b2db45ed8bccd932b4ec4e412daa9742d
kokosko <kostya.kokosko@gmail.com>
2015-10-22 15:15:38 +0300

    fix order products

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,5 +1,5 @@
 class OrderProduct < ActiveRecord::Base
-  validates :order, :size, :color, :product, presence: true
+  validates :order, :color, :product, presence: true
 
   belongs_to :order
   belongs_to :product
