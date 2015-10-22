534c4d3c708720d5db73cc39861e825ed2944a00
kokosko <kostya.kokosko@gmail.com>
2015-10-22 15:27:20 +0300

    fix size name

--- app/models/size.rb
+++ app/models/size.rb
@@ -1,12 +1,14 @@
 class Size < ActiveRecord::Base
-  validates :height, :width, :length, numericality: { greater_than: 0 }, allow_blank: true
+  validates :height, :width, :length, numericality: { greater_than: 0 }, \
+                                      allow_blank: true
   belongs_to :product_group
   belongs_to :product
 
   def name
-    name = length ? (length.to_s + '(' + I18n.t('size.l') + ') x ') : ''
-    name += (width.to_s + '(' + I18n.t('size.w') + ')') if width
-    name += (' x ' + height.to_s + '(' + I18n.t('size.h') + ')') if height
-    name
+    a = []
+    a.push(length.to_s + I18n.t('size.l')) if length
+    a.push(width.to_s + I18n.t('size.w')) if width
+    a.push(height.to_s + I18n.t('size.h')) if height
+    a.compact.join(' x ')
   end
 end
