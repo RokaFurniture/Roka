594cfbc137cdca2ef8f25be8b1147984eba1db67
kokosko <kostya.kokosko@gmail.com>
2015-10-23 23:11:29 +0300

    bundle update, operator can edit materials

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,6 @@ Bundler.require(*Rails.groups)
 module Roka
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
-    # config.middleware.use 'PDFKit::Middleware', print_media_type: true
+    config.middleware.use 'PDFKit::Middleware', print_media_type: true
   end
 end
